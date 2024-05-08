a0b738c9d21849d16a2d9185c372dff3963f696f
Siomermin <fermingonzalez011@gmail.com>
2024-05-08 14:12:46 -0300

    Cambio duracion animacion splash

--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -21,11 +21,11 @@ export class SplashPage implements OnInit {
       this.renderer.addClass(this.h1Element.nativeElement, 'shake-vertical');
       this.renderer.addClass(this.h3Element.nativeElement, 'shake-vertical');
       this.renderer.addClass(this.imgElement.nativeElement, 'wobble-horizontal-bottom');
-    }, 1000); // Add classes after 1 second (1000 milliseconds)
+    }, 3000); // Add classes after 1 second (1000 milliseconds)
 
     setTimeout(() => {
        this.router.navigateByUrl('auth/login');
-    }, 3000);
+    }, 4000);
 
 
   }
